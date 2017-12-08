7401cedd00109bacd4bf63b6073691cc0f2abfb1
Joel Ahlgren <erik.joel.ahlgren@gmail.com>
2017-12-08 01:03:42 +0100

    Giving up on 7.2 for now, committing current "progress" for later.

--- problems/day7.js
+++ problems/day7.js
@@ -1,21 +1,21 @@
 function ParseInput(input)
 {
-    var lines = input.split(/\n+/)
-
+    var lines = input.split(/\n+/g)
     var nodes = {}
 
     lines.forEach(line => 
     {
         var tmp = line.split(' ')
         var node = {Name: tmp[0], Weight: Number(tmp[1].slice(1, -1))}
-
+        
         if (tmp[2] != undefined)
         {
             node.Children = []
 
             for (var i = 3; i < tmp.length; i++)
             {
-                node.Children.push(tmp[i].replace(',', ''))
+                // Clean up the children
+                node.Children.push(tmp[i].replace(',', '').trim())
             }
         }
 
@@ -26,10 +26,11 @@ function ParseInput(input)
     {
         var node = nodes[key]
         if (node.Children != undefined)
-        {
+        {            
+            // Set parent for each child.
             node.Children.forEach(child => 
-            {
-                nodes[child].Parent = node
+            {   
+                nodes[child].Parent = node.Name
             })
         }
     }
@@ -50,10 +51,31 @@ function FindRoot(tree)
     }
 }
 
+function CalculateWeight(tree, node)
+{
+    var weight = 0
+    var currentNode = tree[node]
+
+    if (currentNode.Children != undefined)
+    {
+        currentNode.Children.forEach(child => 
+        {
+            weight += CalculateWeight(tree, child)
+        })        
+    }
+
+    return weight + currentNode.Weight
+}
+
 var testData = "pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)"
 var testTree = ParseInput(testData)
-console.log("Test case 1: " + (FindRoot(testTree) == "tknk"))
+var testTreeRoot = FindRoot(testTree)
+console.log("Test case 1: " + (testTreeRoot == "tknk"))
+console.log("Test case 2.1: " + (CalculateWeight(testTree, testTreeRoot) == 778))
+console.log("Test case 2.2: " + (CalculateWeight(testTree, "fwft") == 243))
 
 var problemInput = require('..\\common\\utils.js').getInput(7)
 var actualTree = ParseInput(problemInput)
-console.log("Root of the tree: " + FindRoot(actualTree))
+var actualTreeRoot = FindRoot(actualTree)
+console.log("Root of the tree: " + actualTreeRoot)
+console.log("Total weight of tree: " + (CalculateWeight(actualTree, actualTreeRoot)))
